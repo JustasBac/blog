ed220c980e9c886ad5c3f6215b1853f489ac9820
JustasBac <justas.bac@outlook.com>
2021-04-15 16:07:54 +0200

    port pakeistas

--- app.js
+++ app.js
@@ -269,7 +269,7 @@ app.get('/generate', async(req, res) => {
 })
 
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
